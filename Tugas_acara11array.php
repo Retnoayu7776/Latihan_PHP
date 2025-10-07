52652a82899f0e06162ae29be5d5edc606bdd3e7
Retnoayu7776 <sayaretno844@gmail.com>
2025-10-07 13:35:40 +0700

    latihanacara11array

---
+++ Tugas_acara11array.php
@@ -0,0 +1,50 @@
+<?php
+// Matriks A
+$A = array(
+    array(1, 1, 1),
+    array(2, 2, 2),
+    array(3, 3, 3)
+);
+
+// Matriks B
+$B = array(
+    array(3, 3, 3),
+    array(2, 2, 2),
+    array(1, 1, 1)
+);
+
+// Matriks hasil (C)
+$C = array();
+
+// Proses penjumlahan dengan looping
+for ($i = 0; $i < 3; $i++) {
+    for ($j = 0; $j < 3; $j++) {
+        $C[$i][$j] = $A[$i][$j] + $B[$i][$j];
+    }
+}
+
+// Tampilkan hasil
+echo "<h3>Matriks A:</h3>";
+for ($i = 0; $i < 3; $i++) {
+    for ($j = 0; $j < 3; $j++) {
+        echo $A[$i][$j] . " ";
+    }
+    echo "<br>";
+}
+
+echo "<h3>Matriks B:</h3>";
+for ($i = 0; $i < 3; $i++) {
+    for ($j = 0; $j < 3; $j++) {
+        echo $B[$i][$j] . " ";
+    }
+    echo "<br>";
+}
+
+echo "<h3>Hasil Penjumlahan (Matriks C = A + B):</h3>";
+for ($i = 0; $i < 3; $i++) {
+    for ($j = 0; $j < 3; $j++) {
+        echo $C[$i][$j] . " ";
+    }
+    echo "<br>";
+}
+?>
